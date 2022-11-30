6231ba8b3e3916793560335684ef5129ff67a2b8
eldoccc <adtibau@gmail.com>
2022-11-30 17:34:15 +0100

    test remi if kaput

--- src/model/states/InGame.java
+++ src/model/states/InGame.java
@@ -1,6 +1,7 @@
 package model.states;
 
 import model.command.BaseCommand;
+import model.command.SendLetterCommand;
 
 public class InGame extends Etat {
 
@@ -8,8 +9,25 @@ public class InGame extends Etat {
     private BaseCommand sendMessageToOtherPlayerCommand;
     private BaseCommand guessWordCommand;
 
+    private int letterState;
+    /*
+     0 = already use
+     1 = letter match
+     2 = lose
+     3 = win
+     4 = letter not match
+    */
+    private int triesLeft;
+    private String hiddenWord;
+
+
     public InGame() {
         super();
+        this.sendLetterCommand = new SendLetterCommand(this.quitCommand);
+        this.sendMessageToOtherPlayerCommand = new SendLetterCommand(this.sendLetterCommand);
+        this.guessWordCommand = new SendLetterCommand(this.sendMessageToOtherPlayerCommand);
+        this.command_available = sendLetterCommand;
+
     }
 
     @Override
@@ -22,4 +40,30 @@ public class InGame extends Etat {
     public String toString() {
         return "InGame";
     }
+
+    public int getLetterState() {
+        return letterState;
+    }
+
+    public void setLetterState(int letterState) {
+        this.letterState = letterState;
+    }
+
+    public int getTriesLeft() {
+        return triesLeft;
+    }
+
+    public void setTriesLeft(int triesLeft) {
+        this.triesLeft = triesLeft;
+    }
+
+    public String getHiddenWord() {
+        return hiddenWord;
+    }
+
+    public void setHiddenWord(String hiddenWord) {
+        this.hiddenWord = hiddenWord;
+    }
 }
+
+
